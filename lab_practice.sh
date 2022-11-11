9481e5109d71a60497088553892cde95ded7a019
Sayed-Anawer <sayedanawer99@gmail.com>
2022-11-12 00:41:30 +0600

    Lab manual 2 lab practice added

---
+++ lab_practice.sh
@@ -0,0 +1,23 @@
+# for i in 1 2 3 4 5
+# do
+#     echo "Welcome $i times"
+# done
+# for i in {1..5}
+# do 
+#     echo "Welcome $i times"
+# done
+
+# for i in {0..10..2}
+# do 
+#     echo "Welcome $i times"
+# done
+
+# for i in $(seq 1 2 20)
+# do 
+#     echo "Welcome $i times"
+# done
+
+# for ((c=1;c<=5;c++))
+# do
+#     echo "Welcome $c times"
+# done
