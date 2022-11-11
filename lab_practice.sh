4eef9d834e42483dc461d2d459293fb0272f4d8c
Sayed-Anawer <sayedanawer99@gmail.com>
2022-11-12 01:07:44 +0600

    added array practice

--- lab_practice.sh
+++ lab_practice.sh
@@ -1,3 +1,7 @@
+# Lab practice 2
+################################
+
+
 # for i in 1 2 3 4 5
 # do
 #     echo "Welcome $i times"
@@ -21,3 +25,40 @@
 # do
 #     echo "Welcome $c times"
 # done
+
+
+#Arrays in shell script
+
+# arr=(prakhar ankit 1 rishabh manish abhinav)
+# echo ${arr[@]} #prints all elements of array
+# echo ${arr[*]} #prints all elements of array
+# echo ${arr[@]:0} #prints all elements of array
+# echo ${arr[*]:0} #prints all elements of array
+
+
+# echo ${arr[0]} #To print first element
+# echo ${arr} #To print first element
+
+
+# echo ${arr[@]:2:3} #To print elements from index 2 to (3) is range
+
+# arr=(1 2 3 4 5 6 7 8 9 10)
+
+# for i in "${arr[@]}"
+# do 
+#     echo $i
+# done
+
+# arr=(1 2 3 4 5 6 7 8 9 10)
+# even=0
+# odd=0
+# for i in "${arr[@]}"
+# do 
+#     if [ $((i%2)) -eq 0 ]
+#     then
+#         even=$((even+i))
+#     else
+#         odd=$((odd+i))
+#     fi
+# done
+# echo "Sum of even numbers is $even and sum of odd numbers is $odd"
